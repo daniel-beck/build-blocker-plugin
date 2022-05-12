0daeac364aac0140516a96e80cd2ac314359f0bd
Daniel Beck <daniel-beck@users.noreply.github.com>
2022-05-12 18:43:28 +0200

    Actually persist configuration when saving

--- src/main/java/io/jenkins/plugins/buildblocker/BuildBlockerConfiguration.java
+++ src/main/java/io/jenkins/plugins/buildblocker/BuildBlockerConfiguration.java
@@ -20,5 +20,6 @@ public class BuildBlockerConfiguration extends GlobalConfiguration {
     @DataBoundSetter
     public void setBlockScheduling(boolean blockScheduling) {
         this.blockScheduling = blockScheduling;
+        save();
     }
 }
